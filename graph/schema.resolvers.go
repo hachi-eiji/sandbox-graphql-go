59f8da0f19a1dc3d03c85a2bfea007b356a70b15
Eiji Hachiya <hachiyae@gmail.com>
2023-05-28 10:13:19 +0900

    feat: authorsの実装

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,16 +6,49 @@ package graph
 
 import (
 	"context"
-	"fmt"
 	"http-go-sandbox/graph/model"
+	"http-go-sandbox/sqls"
+	"strconv"
+
+	pgx "github.com/jackc/pgx/v5"
 )
 
 // Authors is the resolver for the authors field.
 func (r *queryResolver) Authors(ctx context.Context) ([]*model.Author, error) {
-	panic(fmt.Errorf("not implemented: Authors - authors"))
+	conn, err := pgx.Connect(context.Background(), databaseConn())
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer conn.Close(ctx)
+
+	db := sqls.New(conn)
+
+	authors, err := db.ListAuthors(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, a := range authors {
+		author := &model.Author{ID: strconv.FormatInt(a.ID, 10), Name: a.Name}
+		r.authors = append(r.authors, author)
+	}
+
+	return r.authors, nil
 }
 
 // Query returns QueryResolver implementation.
 func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
 
 type queryResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//   - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//     it when you're done.
+//   - You have helper methods in this file. Move them out to keep these resolver files clean.
+func databaseConn() string {
+	return "postgres://postgres:example@127.0.0.1:5431/postgres"
+}
